de161cb9127dc07b99ab9904c35ff30108006acf
brute4s99 <piyushaggarwal002@gmail.com>
2017-10-22 02:56:14 +0530

    Auto Upload

--- GitBot.bat
+++ GitBot.bat
@@ -8,23 +8,29 @@ choice /C abcde /N /M "-> "
 if %errorlevel% ==1 (
 	cd codes
 	del *.exe
+	cls
 	call :upload
+	call :done
 	goto :eof)
 if %errorlevel% ==2 (
 	cd DotCTheProgrammingClub
 	cd assignment_sols
 	del *.exe
+	cls
 	call :upload
 	cd ..
 	cd in_sessions
 	del *.exe
+	cls
 	call :upload
 	cd ..
+	call :done
 	goto :eof
 ) 
 if %errorlevel% ==3 (
 	cd games
 	call :upload
+	call :done
 	goto :eof
 ) 
 if %errorlevel% == 4 (
@@ -41,19 +47,34 @@ if %errorlevel% == 4 (
 	cd misc
 	call :upload
 	cd ..
+	call :done
 	goto :eof
 ) 
 if %errorlevel% == 5 (
 	cd scripts
 	call :upload
+	call :done
 	goto :eof
 )
 
-:upload
- pwd
- git pull
- git add -A
- git commit -m "Auto Upload"
- git push origin master
+:done 
+ sleep 1s
+ echo Everything's all set up, boss.
  sleep 2s
- cls
+ goto :eof
+:upload
+ echo CURRENT DIRECTORY-
+ pwd
+ echo PULLING LATEST REMOTE BRANCH
+ git pull
+ echo DONE.
+ echo ADDING FILES AND
+ git add -A
+ echo COMMITTING CHANGES
+ git commit -m "Auto Upload"
+ choice /N /M "Ready ? : "
+ if %errorlevel% == 1 (
+ echo GOING LIVE...
+ git push origin master
+ )
+ sleep 2s
