3c2689123e263de8e5c5edd913de8efcaac822fc
brute4s99 <piyushaggarwal002@gmail.com>
2018-02-19 15:51:40 +0530

    Uploaded via GitBot v0.5

--- BATCH/Downloads.bat
+++ BATCH/Downloads.bat
@@ -1,8 +1,7 @@
-<<<<<<< HEAD:Downloads.bat
+
 @echo off
-cd ../../Downloads/ 
-=======
-@echo off
-cd x:/Downloads/ 
->>>>>>> 1624e90fa265a1acc6c701e57d9088c732641cfe:BATCH/Downloads.bat
+cd ..
+cd ..
+cd ..
+cd Downloads 
 start .
