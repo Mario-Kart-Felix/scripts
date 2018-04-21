27731964e9589b50fc84e8f7ee1a69dc6b97dfbb
brute4s99 <piyushaggarwal002@gmail.com>
2018-04-21 22:48:44 +0530

    Smarter and vary of PWD now!

--- Python/IMDB-SORT.py
+++ Python/IMDB-SORT.py
@@ -15,6 +15,9 @@ API_Key="XXXXXXXX" 				# ******IMPORTANT PART***** ENTER YOUR API KEY HERE !!!!!
 # $. Run the Script, baby !!!
 noise=["[","]","{","}","(",")",",","DVDRip","mkv","mp4","avi","AAC","RARBG","UNRATED","18+","WEB","DVDScr","XVID","AC3","HQ","HD","Hive",".","_","\\","/","\"","\"","1080","1080p","720","720p","480","480p","360","360p","144","144p","ETRG","BRRip","Blu","Ray"]
 print os.getcwd()
+if (((os.getcwd()).replace("/"," ")).split()[-1]).lower() !='movies':
+	print "start the script from MOVIES folder !"
+	exit()
 err=[]
 for i in range(6,10):
     if not os.path.isdir(str(i)):
@@ -31,30 +34,36 @@ for name in os.listdir("./NEW"):
 			break
 		else:
 			ndx+=1+len(s)
-	print name[:ndx-1]
-	movie_name = name[:ndx-1]
-	movie_name=movie_name.replace(" ","+")
-	print (movie_name+" -- Trying... ")
-	imdb=requests.get("https://www.omdbapi.com/?"+"t="+movie_name+"&apikey="+API_Key)
+	name=name.lower()[:ndx-1]
+	print name
+	query_name = name
+	query_name=query_name.replace(" ","+")
+	imdb=requests.get("https://www.omdbapi.com/?"+"t="+query_name+"&apikey="+API_Key)
 	if imdb.status_code==200:
-		if imdb.json()['Response']=='True':
+		if ("and" in query_name.lower()) and (imdb.json()['Response'] =="False") :
+			query_name=name
+			query_name = query_name.replace("and","&")
+			query_name=query_name.replace(" ","+")
+			imdb=requests.get("https://www.omdbapi.com/?"+"t="+query_name+"&apikey="+API_Key)
+		imdb=imdb.json()
+		if imdb['Response']=='True':
 			print "SUCCESS :-"
-			print imdb.json()['Title']+" - ",
-			print imdb.json()['imdbRating']
-			rating=int(float(imdb.json()['imdbRating']))
-			title=imdb.json()['Title'].replace(":",'-')
+			print imdb['Title']+" - ",
+			print imdb['imdbRating']
+			rating=int(float(imdb['imdbRating']))
+			title=imdb['Title'].replace(":",'-')
 			if not os.path.isdir("./NEW/"+orig_name):
 				os.mkdir("./NEW/"+title)
-				shutil.move("./NEW"+orig_name,"./NEW/"+title)
+				shutil.move("./NEW/"+orig_name,"./NEW/"+title)
 				orig_name=title 
 			if(rating<6):
 				shutil.move("./NEW/"+orig_name,"Below 6/"+title)
 			else:
 				shutil.move("./NEW/"+orig_name,str(rating)+"/"+title)
 		else:
-			err.append(str(movie_name)+" NOT IN IMDB DATABASE")
+			err.append(str(query_name)+" NOT IN IMDB DATABASE")
 	else:
-		err.append(movie_name)
+		err.append(query_name)
 print str(len(err))+" Movies Not Found "
 for i in err:
     print i
